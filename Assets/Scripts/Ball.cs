79be91b4948c12f9c359c3c46ba967fcb957b7a7
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-10 15:03:33 -0300

    Ball script creation.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 8f;
+        Movement();      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Movement()
+    {
+        float speedX = Random.Range(0, 2) == 0 ? -1 : 1;
+        float speedY = Random.Range(0, 2) == 0 ? -1 : 1;
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * speedX, speed * speedY, 0f);
+    }
+}
