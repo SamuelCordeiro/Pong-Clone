29ec42137815ac609d80903219cbe6332a0ce336
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-10 15:04:19 -0300

    Platforms script creation.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float speed;
+    public bool isRight;
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if(isRight)
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0f);
+        }
+        else
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical 2") * speed * Time.deltaTime, 0f);
+        }
+    }
+}
