f0bf6cd16d662dca759668b320b88b4b148413b0
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-11 01:13:29 -0300

    Sides script creation.

---
+++ Assets/Scripts/Sides.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sides : MonoBehaviour
+{
+    public bool isRight;
+    //private int leftScore;
+    //private int rightScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag == "Ball" && isRight)
+        {
+            GameController.instace.ScoreUpdate("left");
+        }
+        else
+        {
+            GameController.instace.ScoreUpdate("right");
+        }
+    }
+}
